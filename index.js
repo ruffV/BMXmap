da82f7b1f3591ce03750e3f36b9f3d49595c2074
mrvans123456789 <maxwellvale@icloud.com>
2023-01-09 00:23:49 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+ function initMap(){
+      var options = {
+        zoom:8,
+        center:{lat:30.2672,lng:-97.7431}
+      }
+
+      var map = new google.maps.Map(document.getElementById('map'), options);
+      
+      const contentString =
+    'YUHHHHHH<img src="resources/test.png">';
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      ariaLabel: "Uluru",
+    });
+
+      var marker = new google.maps.Marker({
+        position:{lat:30.28058939021873, lng:-97.81840654782212},
+        map:map
+      })    
+
+      marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+
+}
